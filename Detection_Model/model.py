fb5faede4c4daefcdde7277d7d676357dc1e3829
Emp231 <101529038+Emp231@users.noreply.github.com>
2023-07-27 19:05:12 -0700

    add classification method

--- Detection_Model/model.py
+++ Detection_Model/model.py
@@ -1,32 +1,26 @@
 import cv2
-import tensorflow as tf
-import os
 from flask import Flask, request, jsonify
+import werkzeug
+import base64
 
-app = Flask("Somnolence")
+app = Flask(__name__)
+@app.route("/detect", methods=["POST"])
+def detect():
+    pass
 
-def upload():
-    video_file = request.files["video"]
-    video_file.save("Detection_Model/Video/video.mp4")
-    return jsonify({"message": "Video recieved and processed successfully!"})
 
 if __name__ == "__main__":
     app.run()
 
 
-prediction = None
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
-
-vid = cv2.VideoCapture("Detection_Model/Video/video.mp4")
-
-while vid.isOpened():
-    ret, frame = vid.read()
+def perform_classification(frame):
+    prediction = None
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    if ret:
-        for (x, y, w, h) in faces:
+    for (x, y, w, h) in faces:
             prediction = "Closed"
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 5)
             ROI_G = gray[y:y+w, x:x+w]
@@ -38,12 +32,4 @@ while vid.isOpened():
 
                 cv2.rectangle(ROI_C, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 5)
 
-        cv2.imshow("Somnolence", frame)
-        print(prediction)
-    else:
-        break
-
-    
-vid.release()
-cv2.destroyAllWindows()
-os.remove("Detection_Model/Video/video.mp4")
+    return prediction
